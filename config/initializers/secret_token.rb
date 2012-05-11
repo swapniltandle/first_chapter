eb48cac72cb2defe38dde0388f2ebdcffc5d6573
swapnil <swapnil.tandle3>
2012-05-11 19:06:23 +0530

    initial 3rd chapter comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3e9a90be1f1fa3a47e60a6ef94f51176fd051672e06d130db3874a4c830b701d5fe4ad3561bdcd02473bb6de02c3e1c2768672a12088d6d3fcee05364da99a90'
